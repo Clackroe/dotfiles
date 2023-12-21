36aec5a7184033a8809327c6c18084ac1593cba6
Clackroe <clackroe@icloud.com>
2023-12-21 17:15:41 -0500

    Updated zsh install flow

--- configs/zsh/config.sh
+++ configs/zsh/config.sh
@@ -2,6 +2,8 @@
 
 
 echo Linking ZSH configurations..
-ln -sf "$DOTFILES_DIR/configs/zsh/.oh-my-zsh" ~/
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+rm -rf ~/.zshrc
 ln -sf "$DOTFILES_DIR/configs/zsh/.zshrc" ~/
 
