79690fbefe3acc6829dc0db5ef157a67e778b130
Clackroe <clackroe@gmail.com>
2023-12-22 16:28:47 -0500

    Update plugins zsh)

--- zsh/config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo Linking ZSH configurations..
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-rm -rf ~/.zshrc
-ln -sf "$DOTFILES_DIR/configs/zsh/.zshrc" ~/
-
