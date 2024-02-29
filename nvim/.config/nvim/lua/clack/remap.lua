c57edf850524472bfc6de94635025af0ff6a74f8
Clackroe <clackroe@gmail.com>
2024-02-29 16:43:44 -0500

    Switched to new and better version of lazy, and modified some settings for easier use. Also this is an ammend.

--- nvim/.config/nvim/lua/clack/remap.lua
+++ nvim/.config/nvim/lua/clack/remap.lua
@@ -23,9 +23,10 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
