422afbe6fc1ecfea8720123391034d378868b381
Clackroe <clackroe@gmail.com>
2024-03-02 20:04:44 -0500

    Remavp < and > to keep your selection

--- nvim/.config/nvim/lua/clack/remap.lua
+++ nvim/.config/nvim/lua/clack/remap.lua
@@ -3,6 +3,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
 
 vim.keymap.set("n", "J", "mzJ`z")
 
@@ -25,7 +27,7 @@ vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
-vim.keymap.set("n", "<leader><cr>", "<cmd>silent !tmux neww -c".. vim.fn.getcwd() .."<CR>")
+vim.keymap.set("n", "<leader><cr>", "<cmd>silent !tmux neww -c" .. vim.fn.getcwd() .. "<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
@@ -34,4 +36,3 @@ vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
