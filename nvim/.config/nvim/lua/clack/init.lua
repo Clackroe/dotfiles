aff79c7a065dc5bb295263e15303254072b50d3b
Clackroe <clackroe@gmail.com>
2024-03-24 20:42:23 -0400

    Feat: ctrl space to switch from source to header file in c++ projects.

--- nvim/.config/nvim/lua/clack/init.lua
+++ nvim/.config/nvim/lua/clack/init.lua
@@ -1,42 +1,65 @@
-require "clack.options"
-require "clack.remap"
-require "clack.lazy_init"
-
+require("clack.options")
+require("clack.remap")
+require("clack.lazy_init")
 
 local augroup = vim.api.nvim_create_augroup
-local clackroe = augroup('ThePrimeagen', {})
+local clackroe = augroup("ThePrimeagen", {})
 local autocmd = vim.api.nvim_create_autocmd
 
 require("telescope").load_extension("git_worktree")
 
-
 -- Worktree stuff
 
 vim.keymap.set("n", "<leader>cd", "<CMD>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>", silent)
-vim.keymap.set("n", "<leader>cD", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>", silent)
+vim.keymap.set(
+	"n",
+	"<leader>cD",
+	"<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
+	silent
+)
 
 local Worktree = require("git-worktree")
 Worktree.on_tree_change(function(op, metadata)
-    if op == Worktree.Operations.Switch then
-        vim.keymap.set("n", "<leader><cr>", "<cmd>silent !tmux neww -c".. vim.fn.getcwd() .."<CR>")
-        print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
-    end
+	if op == Worktree.Operations.Switch then
+		vim.keymap.set("n", "<leader><cr>", "<cmd>silent !tmux neww -c" .. vim.fn.getcwd() .. "<CR>")
+		print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+	end
 end)
 --
 --This is a test for ammending commits
-autocmd('LspAttach', {
-    group = clackroe,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    end
+autocmd("LspAttach", {
+	group = clackroe,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>vws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		vim.keymap.set("n", "<leader>vca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<leader>vrn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
+	end,
 })
