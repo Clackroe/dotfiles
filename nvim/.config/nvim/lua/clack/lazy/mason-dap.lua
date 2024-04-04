00ede873e73a280d99dda050f258e441966342c7
Clackroe <clackroe@gmail.com>
2024-04-04 17:04:38 -0400

    feat: remove Worktrees, add CMakeTools

--- nvim/.config/nvim/lua/clack/lazy/mason-dap.lua
+++ nvim/.config/nvim/lua/clack/lazy/mason-dap.lua
@@ -1,71 +1,65 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"jay-babu/mason-nvim-dap.nvim",
-		"williamboman/mason.nvim",
-	},
-	opts = {
-		handlers = {},
-		ensure_installed = {
-			"codelldb",
-		},
-	},
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "jay-babu/mason-nvim-dap.nvim",
+        "williamboman/mason.nvim",
+    },
+    opts = {
+        handlers = {},
+        ensure_installed = {
+            "codelldb",
+        },
+    },
 
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		local mason = require("mason")
-		local masondap = require("mason-nvim-dap")
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        local mason = require("mason")
+        local masondap = require("mason-nvim-dap")
 
-		mason.setup()
-		masondap.setup({
-			handlers = {},
-		})
+        mason.setup()
+        masondap.setup({
+            handlers = {},
+        })
 
-		dapui.setup()
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+        dapui.setup()
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
 
-		vim.keymap.set("n", "<leader>bb", function()
-			require("dap").toggle_breakpoint()
-		end)
-		vim.keymap.set("n", "<leader>br", function()
-			require("dap").continue()
-		end)
+        vim.keymap.set("n", "<leader>bb", function()
+            require("dap").toggle_breakpoint()
+        end)
+        vim.keymap.set("n", "<leader>br", function()
+            require("dap").continue()
+        end)
+        vim.keymap.set("n", "<Leader>bq", ":lua require('dap').terminate()<CR>")
 
-		vim.keymap.set("n", "<F4>", ":lua require('dapui').toggle()<CR>")
+        vim.keymap.set("n", "<Leader>bc", ":lua require('dap').continue()<CR>")
+        vim.keymap.set("n", "<Leader>bo", ":lua require('dap').step_over()<CR>")
+        vim.keymap.set("n", "<Leader>bi", ":lua require('dap').step_into()<CR>")
+        vim.keymap.set("n", "<Leader>bO", ":lua require('dap').step_out()<CR>")
+        vim.keymap.set("n", "<Leader>bu", ":lua require('dapui').toggle()<CR>")
 
-		vim.keymap.set("n", "<F10>", ":lua require('dap').step_over()<CR>")
-		vim.keymap.set("n", "<F11>", ":lua require('dap').step_into()<CR>")
-		vim.keymap.set("n", "<F12>", ":lua require('dap').continue()<CR>")
-		-- vim.keymap.set("n", "<F3>", ":lua require('dap').step_out()<CR>")
+        vim.keymap.set("n", "<Leader>bK", ":lua require('dap.ui.variables').hover()<CR>")
+        vim.keymap.set("v", "<Leader>bv", ":lua require('dap.ui.variables').visual_hover()<CR>")
+        --
+        --
+        -- vim.keymap.set( "n", "<Leader>dro", ":lua require('dap').repl.open()<CR>" )
+        -- vim.keymap.set( "n", "<Leader>drl", ":lua require('dap').repl.run_last()<CR>" )
+        --
 
-		vim.keymap.set("n", "<Leader>bc", ":lua require('dap').continue()<CR>")
-		vim.keymap.set("n", "<Leader>bo", ":lua require('dap').step_over()<CR>")
-		vim.keymap.set("n", "<Leader>bi", ":lua require('dap').step_into()<CR>")
-		vim.keymap.set("n", "<Leader>bO", ":lua require('dap').step_out()<CR>")
-		vim.keymap.set("n", "<Leader>bu", ":lua require('dapui').toggle()<CR>")
-
-		-- vim.keymap.set( "n", "<Leader>dhh", ":lua require('dap.ui.variables').hover()<CR>" )
-		-- vim.keymap.set( "v", "<Leader>dhv", ":lua require('dap.ui.variables').visual_hover()<CR>" )
-		--
-		--
-		-- vim.keymap.set( "n", "<Leader>dro", ":lua require('dap').repl.open()<CR>" )
-		-- vim.keymap.set( "n", "<Leader>drl", ":lua require('dap').repl.run_last()<CR>" )
-		--
-
-		-- vim.keymap.set( "n", "<Leader>dc", ":lua require('dap.ui.variables').scopes()<CR>" )
-	end,
+        -- vim.keymap.set( "n", "<Leader>dc", ":lua require('dap.ui.variables').scopes()<CR>" )
+    end,
 }
