f5ff0b0a2a9ab4f93c35accf54fb40d0163836ab
Clackroe <clack@clack.clack>
2025-05-05 03:26:16 -0400

    Customize polybar

--- init.py
+++ init.py
@@ -30,7 +30,7 @@ for f in os.listdir("install-scripts"):
 os.system(f"./install-scripts/end")
 
 
-STOW_DIRS = ["i3", "zsh", "flameshot", "wezterm"]
+STOW_DIRS = ["i3", "zsh", "flameshot", "wezterm", "polybar"]
 
 for st in STOW_DIRS:
     print(f'Stowing {st}')
